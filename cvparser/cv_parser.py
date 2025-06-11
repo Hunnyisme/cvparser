0a3cf778e81a497c8b356a4bee200b193b833d93
zhengjie <jetchill521@gmail.com>
2025-06-11 13:28:06 +1000

    small changes

--- cvparser/cv_parser.py
+++ cvparser/cv_parser.py
@@ -1,7 +1,10 @@
 import re
 from pathlib import Path
+
+from cvparser.factories.extractor_factory import ExtractorFactory
 from cvparser.factories.parser_factory import *
 from cvparser.Enum.filetype import *
+from cvparser.parser.doc import Doc
 
 
 class CvParser:
@@ -17,5 +20,14 @@ class CvParser:
         except Exception as e:
             print(e)
             return None
+    @staticmethod
+    def extract(doc:Doc,country,language:Language=None):
+       '''
+
+
+       :param language: if this parameter is None, program will try to detect language
+
+       '''
+       return ExtractorFactory.get_extractor(doc,country,language).extract()
 
 
